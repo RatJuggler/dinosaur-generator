060eb84eb50586e6efc1105275ac0b957b07669a
John Chase <ratteal@gmail.com>
2019-10-15 13:16:57 +0100

    Read in existing names to use for generation.

--- dinogen/__main__.py
+++ dinogen/__main__.py
@@ -1,5 +1,27 @@
+import pathlib
+
+
+def read_existing_names():
+    existing_names = pathlib.Path(__file__).parent / "dinosaurs.csv"
+    with existing_names.open('r', encoding='utf-8') as f:
+        raw_text = f.read()
+    return raw_text.lower()
+
+
+def model(name_text, ix_to_char, char_to_ix):
+    print("TODO Model...")
+
+
 def generate():
-    print("Generator skeleton...");
+    name_text = read_existing_names()
+    name_chars = list(set(name_text))
+    text_size, vocab_size = len(name_text), len(name_chars)
+    print('There are %d total characters and %d unique characters in your data.' % (text_size, vocab_size))
+    char_to_ix = {ch: i for i, ch in enumerate(sorted(name_chars))}
+    ix_to_char = {i: ch for i, ch in enumerate(sorted(name_chars))}
+    print(char_to_ix)
+    print(ix_to_char)
+    model(name_text, ix_to_char, char_to_ix)
 
 
 if __name__ == "__main__":
