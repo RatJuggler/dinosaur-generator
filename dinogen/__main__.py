a37c5b84bffa1522fd2e857e060cdb9394686793
John Chase <ratteal@gmail.com>
2019-10-15 15:57:16 +0100

    Clarify variable names.

--- dinogen/__main__.py
+++ dinogen/__main__.py
@@ -3,25 +3,26 @@ import pathlib
 from .model import model
 
 
-def read_sample_file():
-    sample_file = pathlib.Path(__file__).parent / "dinosaurs.csv"
-    with sample_file.open('r', encoding='utf-8') as f:
-        sample_names = f.readlines()
-    sample_names = [x.lower().strip() for x in sample_names]
-    return sample_names
+def read_training_file():
+    training_file = pathlib.Path(__file__).parent / "dinosaurs.csv"
+    with training_file.open('r', encoding='utf-8') as f:
+        training_names = f.readlines()
+    training_names = [x.lower().strip() for x in training_names]
+    return training_names
 
 
 def generate():
-    sample_names = read_sample_file()
-    name_chars = set('\n'.join(sample_names))
-    text_size, vocab_size = len(sample_names), len(name_chars)
-    print("There are {0} total characters and {1} unique characters in the sample name data."
-          .format(text_size, vocab_size))
-    char_to_index = {ch: i for i, ch in enumerate(sorted(name_chars))}
-    index_to_char = {i: ch for i, ch in enumerate(sorted(name_chars))}
+    training_names = read_training_file()
+    training_text = '\n'.join(training_names)
+    training_chars = set(training_text)
+    vocab_size = len(training_chars)
+    print("There are {0} names with {1} total characters and {2} unique characters in the training data."
+          .format(len(training_names), len(training_text), vocab_size))
+    char_to_index = {ch: i for i, ch in enumerate(sorted(training_chars))}
+    index_to_char = {i: ch for i, ch in enumerate(sorted(training_chars))}
     print(char_to_index)
     print(index_to_char)
-    model(sample_names, index_to_char, char_to_index, len(name_chars), 50, 7, 40000)
+    model(training_names, index_to_char, char_to_index, vocab_size, 50, 7, 35000)
 
 
 if __name__ == "__main__":
