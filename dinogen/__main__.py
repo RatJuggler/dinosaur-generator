b015285799ced01f4382d1d9f2591d0a41d98e6f
John Chase <ratteal@gmail.com>
2019-10-15 15:03:15 +0100

    Refactor to read in sample names once.

--- dinogen/__main__.py
+++ dinogen/__main__.py
@@ -1,27 +1,27 @@
 import pathlib
 
-
-def read_existing_names():
-    existing_names = pathlib.Path(__file__).parent / "dinosaurs.csv"
-    with existing_names.open('r', encoding='utf-8') as f:
-        raw_text = f.read()
-    return raw_text.lower()
+from .model import model
 
 
-def model(name_text, ix_to_char, char_to_ix):
-    print("TODO Model...")
+def read_sample_file():
+    sample_file = pathlib.Path(__file__).parent / "dinosaurs.csv"
+    with sample_file.open('r', encoding='utf-8') as f:
+        sample_names = f.readlines()
+    sample_names = [x.lower().strip() for x in sample_names]
+    return sample_names
 
 
 def generate():
-    name_text = read_existing_names()
-    name_chars = list(set(name_text))
-    text_size, vocab_size = len(name_text), len(name_chars)
-    print('There are %d total characters and %d unique characters in your data.' % (text_size, vocab_size))
-    char_to_ix = {ch: i for i, ch in enumerate(sorted(name_chars))}
-    ix_to_char = {i: ch for i, ch in enumerate(sorted(name_chars))}
-    print(char_to_ix)
-    print(ix_to_char)
-    model(name_text, ix_to_char, char_to_ix)
+    sample_names = read_sample_file()
+    name_chars = set('\n'.join(sample_names))
+    text_size, vocab_size = len(sample_names), len(name_chars)
+    print("There are {0} total characters and {1} unique characters in the sample name data."
+          .format(text_size, vocab_size))
+    char_to_index = {ch: i for i, ch in enumerate(sorted(name_chars))}
+    index_to_char = {i: ch for i, ch in enumerate(sorted(name_chars))}
+    print(char_to_index)
+    print(index_to_char)
+    model(sample_names, index_to_char, char_to_index, len(name_chars))
 
 
 if __name__ == "__main__":
